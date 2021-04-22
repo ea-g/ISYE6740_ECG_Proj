d88c251e8e65e338f1641a2b3408214369fe19c1
ea-g <58582043+ea-g@users.noreply.github.com>
2021-04-22 18:54:49 +0900

    parameter edits, new outputs

--- Code/minirocket_wavelet_compare.py
+++ Code/minirocket_wavelet_compare.py
@@ -8,9 +8,8 @@ Created on Thu Apr 22 13:52:18 2021
 from sklearn.model_selection import GridSearchCV
 import get_data
 from sktime.transformations.panel.rocket import MiniRocketMultivariate 
-from sklearn.linear_model import RidgeClassifier, LogisticRegression 
+from sklearn.linear_model import RidgeClassifier, LogisticRegression, SGDClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from wavelet_features import get_ecg_features
@@ -18,6 +17,7 @@ from utils import matrix_2_df, make_gridcv, output_folder
 from collections import defaultdict
 from joblib import dump
 import os
+import matplotlib.pyplot as plt
 
 # prepare data for MiniRocket and sktime models
 X_train_df = matrix_2_df(get_data.X_train)
@@ -34,8 +34,8 @@ X_test_wv = get_ecg_features(get_data.X_test)
 
 # list of models, feel free to add more (for parameters see model_params dict 
 # in utils.py)
-models = [SVC(), LogisticRegression(max_iter=300), GaussianNB(),
-          KNeighborsClassifier(),RandomForestClassifier(), RidgeClassifier(),
+models = [SGDClassifier(max_iter=2000), LogisticRegression(max_iter=300), GaussianNB(),
+          KNeighborsClassifier(), RandomForestClassifier(), RidgeClassifier(),
           AdaBoostClassifier()]
 
 unfit_models = defaultdict(GridSearchCV)
@@ -50,7 +50,5 @@ fit_models = defaultdict(GridSearchCV)
 for name, clf in unfit_models.items():
     for training, label in zip([X_train_mr, X_train_wv], ['MiniR', 'Wav']):
         fit_models[name+'_'+label] = clf.fit(training, get_data.y_train)
-        dump(clf, os.path.join(output_folder, '{}_{}_gridcv.joblib'\
-                               .format(name, label)))
-
+        dump(clf, os.path.join(output_folder, '{}_{}_gridcv.joblib'.format(name, label)))
 
