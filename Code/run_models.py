4172c223a7c04db04caa857ed754781b71ee90e3
ea-g <58582043+ea-g@users.noreply.github.com>
2021-04-29 10:34:59 +0900

    Update run_models.py
    final set up before running

--- Code/run_models.py
+++ Code/run_models.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from wavelet_features import get_ecg_features
-from utils import matrix_2_df, model_wrapper, filter_all
+from utils import matrix_2_df, model_wrapper, filter_all, extract_all_features
 
 to_filter = False
 
@@ -25,6 +25,15 @@ else:
 if to_filter:
     x_train = filter_all(x_train)
     x_val = filter_all(x_val)
+
+    # extract Dr.Ojas features
+    X_train_ecg = extract_all_features(x_train)
+    X_val_ecg = extract_all_features(x_val)
+
+else:
+    # filter only for ECG descriptors
+    X_train_ecg = extract_all_features(filter_all(x_train))
+    X_val_ecg = extract_all_features(filter_all(x_val))
 
 # extract features with wavelet transform
 X_train_wv = pd.DataFrame(data=get_ecg_features(x_train))
@@ -45,11 +54,27 @@ X_val_mr.columns = ['f_' + str(i) for i in X_val_mr.columns]
 X_train_wv.columns = ['f_' + str(i) for i in X_train_wv.columns]
 X_val_wv.columns = ['f_' + str(i) for i in X_val_wv.columns]
 
-# concat patient meta-data features with each of the above
-X_train_metmr = pd.concat([X_train_mr, x_train_meta], axis=1)
-X_val_metmr = pd.concat([X_val_mr, x_val_meta], axis=1)
-X_train_metwv = pd.concat([X_train_wv, x_train_meta], axis=1)
-X_val_metwv = pd.concat([X_val_wv, x_val_meta], axis=1)
+
+feature_data = {'MR': {'train': X_train_mr, 'test': X_val_mr}, 'wavelet': {'train': X_train_wv, 'test': X_val_mr},
+                'meta': {'train': x_train_meta, 'test': x_val_meta}, 'ecg': {'train': X_train_ecg, 'test': X_val_ecg}}
+
+
+def feature_mix(to_mix):
+    """
+    Combines data from different feature extractions into a single dataframe given a list of keys to combine from the
+    feature_data dictionary.
+
+    *args
+    - to_mix:
+        list of keys from the feature_data dictionary to combine
+
+    Returns:
+        train (dataframe of combined features for training), test (dataframe of combined features for testing)
+    """
+    train = pd.concat([feature_data[i]['train'] for i in to_mix], axis=1)
+    test = pd.concat([feature_data[i]['test'] for i in to_mix], axis=1)
+    return train, test
+
 
 # list of models, feel free to add more (for parameters see model_params dict
 # in utils.py), //note : LogisticRegression did not converge, needs more iter
@@ -57,14 +82,21 @@ models = [SGDClassifier(max_iter=2000), LogisticRegression(max_iter=1000), Gauss
           KNeighborsClassifier(), RandomForestClassifier(), RidgeClassifier(),
           AdaBoostClassifier()]
 
-# trials
-raw_MR_meta = model_wrapper(models[2:5], X_train_metmr, get_data.y_train_multi, cat=['sex'], prefix='raw-MR-meta',
-                            scoring="roc_auc_ovr", n_jobs=3)
+# set up models below here ============================================================================================
+# # concat patient meta-data features with each of the above
+# X_train_metmr = pd.concat([X_train_mr, x_train_meta], axis=1)
+# X_val_metmr = pd.concat([X_val_mr, x_val_meta], axis=1)
+# X_train_metwv = pd.concat([X_train_wv, x_train_meta], axis=1)
+# X_val_metwv = pd.concat([X_val_wv, x_val_meta], axis=1)
+#
+# # trials
+# raw_MR_meta = model_wrapper(models[2:5], X_train_metmr, get_data.y_train_multi, cat=['sex'], prefix='raw-MR-meta',
+#                             scoring="roc_auc_ovr", n_jobs=3)
+#
+# raw_wv_meta = model_wrapper(models[2:5], X_train_metwv, get_data.y_train_multi, cat=['sex'], prefix='raw-wv-meta',
+#                             scoring="roc_auc_ovr", n_jobs=3)
+#
+# raw_MR = model_wrapper(models[2:5], X_train_mr, get_data.y_train_multi, prefix='raw-MR',
+#                        scoring="roc_auc_ovr", n_jobs=3)
 
-raw_wv_meta = model_wrapper(models[2:5], X_train_metwv, get_data.y_train_multi, cat=['sex'], prefix='raw-wv-meta',
-                            scoring="roc_auc_ovr", n_jobs=3)
-
-raw_MR = model_wrapper(models[2:5], X_train_mr, get_data.y_train_multi, prefix='raw-MR',
-                            scoring="roc_auc_ovr", n_jobs=3)
-
-#test = gridcv_all(SGDClassifier(max_iter=2000), X_train_metmr.columns, categorical=['sex'])
+# test = gridcv_all(SGDClassifier(max_iter=2000), X_train_metmr.columns, categorical=['sex'])
