b54e7517f332647c4ba8c7d57f1ea49df437335d
Dae <phumthep@gmail.com>
2021-04-30 16:07:22 +0700

    Update model_evaluation.py

--- Code/model_evaluation.py
+++ Code/model_evaluation.py
@@ -12,14 +12,12 @@ import pandas as pd
 import scikitplot as skplt
 from collections import defaultdict
 from joblib import dump, load
-from run_models import X_train, X_test
 
 ##################
 
 def plot_multi_ROC(models, X_test, y_test):
     '''Plot the multiple ROC curves for each model
     from https://stackoverflow.com/questions/25009284/how-to-plot-roc-curve-in-python'''
-
     for model in models:
         y_proba = model.predict_proba(X_test)
         skplt.metrics.plot_roc_curve(y_test, y_proba)
@@ -27,6 +25,7 @@ def plot_multi_ROC(models, X_test, y_test):
 
 
 def plot_scores(df):
+    '''Return a barplot comparing model performance'''
     fig, ax = plt.subplots()
     ax = sns.barplot(
         x = 'output_name',
@@ -38,6 +37,27 @@ def plot_scores(df):
     plt.show()
 
 
+def get_performance(outputs):   
+    '''Return a dataframe containing the optimal model info'''
+    frames = []
+    models = []
+    for entry in outputs:
+        model_name = entry.name.split('.')[0]
+        model_output = load(entry)
+        temp_dict = {
+            'output_name': model_name,
+            'best_score': model_output.best_score_,
+            'best_parameters': model_output.best_params_
+            }
+        frames.append(pd.DataFrame(temp_dict))
+        models.append(model_output)
+    model_info_df = pd.concat(frames, ignore_index=True)
+    
+    return model_info_df
+
+
+################## 
+
 # The current working directory should be "..\ISYE6740_ECG_Proj"
 cur_dir = os.getcwd()
 output_folder = cur_dir + '\Output'
@@ -46,26 +66,14 @@ output_folder = cur_dir + '\Output'
 outputs = [entry for entry in os.scandir(output_folder) \
            if not entry.name.startswith('.') and entry.is_file()]
 
-# Create a dataframe containing the optimal model info
-frames = []
-for entry in outputs:
-    model_name = entry.name.split('.')[0]
-    model_output = load(entry)
-    temp_dict = {
-        'output_name': model_name,
-        'best_score': model_output.best_score_,
-        'best_parameters': model_output.best_params_
-        }
-    frames.append(pd.DataFrame(temp_dict))
-    models.append(model_output)
-df = pd.concat(frames, ignore_index=True)
-
+    
+get_performance(outputs)
 
 ################## Need the model input to plot ROC
 
-plot_multi_ROC(models[0], X_train, y_train)
+plot_multi_ROC(models, X_test_meta, y_test)
 
-model = models[0]
-y_proba = model.predict_proba(X_train)
-skplt.metrics.plot_roc_curve(y_train_meta, y_proba)
-plt.show()
+# model = models[3]
+# y_proba = model.predict_proba(X_val_wv)
+# skplt.metrics.plot_roc_curve(y_train_meta, y_proba)
+# plt.show()
